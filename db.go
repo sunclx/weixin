1066ce1f734ebdab260a66e57152f53b1bbaa9dd
sunclx <sunclx@gmail.com>
2016-07-20 18:47:25 +0800

    fix db

--- db.go
+++ db.go
@@ -9,14 +9,4 @@ func db() {
 	}
 	defer db.Close()
 
-	db.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucketIfNotExists([]byte("default"))
-		if err != nil {
-			return err
-		}
-
-		err = b.Put([]byte("oXU74wMt--mr4eaVyhmJ5h_lSJP0"), []byte("09170510"))
-		return err
-	})
-
 }
