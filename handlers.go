5415eb9daf5eb4e18f0b1a86af427f7b107a7284
sunclx <sunclx@gmail.com>
2016-07-29 17:00:35 +0800

    fix

--- handlers.go
+++ handlers.go
@@ -10,13 +10,9 @@ func logHandler(c *Context) {
 	fmt.Println(r.RemoteAddr, r.Method, r.Host, r.URL.Path, r.URL.RawQuery)
 }
 
-func defaultHandler(c *Context) {
-	c.ResponseText("暂不支持此类型信息")
-}
-
 func messageHandler(c *Context) {
 	if c.Message.MsgType != MsgTypeText {
-		defaultHandler(c)
+		c.ResponseText("暂不支持此类型信息")
 		return
 	}
 
@@ -29,9 +25,9 @@ func messageHandler(c *Context) {
 	msg.UseFunc(handleBindPhone)
 	msg.Begin()
 
-	// if msg.buffer.Len() <= 0 {
-	// 	defaultHandler(c)
-	// 	return
-	// }
+	if msg.buffer.Len() <= 0 {
+		c.ResponseText("你的信息格式错误")
+		return
+	}
 	c.ResponseText(msg.buffer.String())
 }
